3cc31ecf8f4026af6fc80e8510148943f65d46a2
borhadeshubham5 <100440125+borhadeshubham5@users.noreply.github.com>
2022-07-06 13:04:16 -0500

    Update connectiondb.php

--- connectiondb.php
+++ connectiondb.php
@@ -1,18 +1,16 @@
 <?php
-    $serverName = "DESKTOP-VIE5VBT\SQLEXPRESS"; 
-    $uid = "DESKTOP-VIE5VBT\Shubham Borhade";   
-    $pwd = "";  
-    $databaseName = "classes_quiz"; 
-    //$connectionInfo = array( "UID"=>$uid, "PWD"=>$pwd, "Database"=>$databaseName); 
-    $connectionInfo = array( "Database"=>"classesquiz"); 
-    /* Connect using SQL Server Authentication. */  
-    $conn = sqlsrv_connect($serverName, $connectionInfo);  
-    if(!$conn)
-    {
-        echo "couldn't connect";
-    }
-    else
-    {
-        //echo "connected";
-    }
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:cse6331ssb.database.windows.net,1433; Database = cse6331ssb", "ssb4235", "2996Shubh@");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "ssb4235", "pwd" => "2996Shubh@", "Database" => "cse6331ssb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:cse6331ssb.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
